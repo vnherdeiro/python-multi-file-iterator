e89d45e80f6a5b8bc83ce540ee28f53ff0bcc7af
Victor Herdeiro <victor.oriedreh@gmail.com>
2019-03-29 15:58:41 +0000

    minor bug fixes

--- MultiFileIterator.py
+++ MultiFileIterator.py
@@ -1,12 +1,10 @@
-from itertools.chain import from_iterator
-
-
 class MultiFileIterator:
 
 	def __init__(self, input_files):
 		if isinstance( input_files, str):
 			input_files = [input_files,]
-		self.iterator = from_iterables(map( lambda x : open(x), input_files))
+		self.iterator = map( lambda x : open(x), input_files)
+		self.input_files = input_files
 
 	def __iter__(self):
 		return self.iterator
@@ -14,9 +12,17 @@ class MultiFileIterator:
 	def __next__(self):
 		return next( self.iterator)
 
-
 	def __enter__(self):
-		return self.iterator
+		return self
 
-	def __exit__(self):
+	def __exit__(self, *args):
 		return
+
+	def read(self):
+		return ''.join( it.read() for it in self.iterator)
+
+	def readlines(self):
+		return [lines for it in self.iterator for lines in it.readlines()]
+
+	def reset(self):
+		self.iterator = map( lambda x : open(x), self.input_files)
